87afdd937ef6b0f909f849409280639fcc54aedc
Leon M. Busch-George <leon@georgemail.eu>
2023-06-12 18:37:40 +0200

    merge bucket with generic id

--- bucket.go
+++ bucket.go
@@ -1,55 +1,34 @@
 package work_sync
 
-// caller chooses value
-type BucketId uint64
+type WorkFunc[Id comparable, State any] func(state *State)
 
-type WorkFunc func()
-type WorkFuncWithState[T any] func(bucketState *T)
-
-type BucketWorkReq struct {
+type BucketWorkReq[Id comparable, State any] struct {
 	// each worker will only handle one request at time
-	Bucket BucketId
-	F WorkFunc
+	Bucket Id
+	F WorkFunc[Id, State]
 }
 
-type BucketWorkReqWithState[T any] struct {
-	Bucket BucketId
-	F WorkFuncWithState[T]
-}
-
-type BucketData struct {
-	work chan WorkFunc
-	jobs *uint
-}
-
-type BucketDataWithState[T any] struct {
-	work chan WorkFuncWithState[T]
+type BucketData[Id comparable, State any] struct {
+	work chan WorkFunc[Id, State]
 	jobs *uint
 }
 
 // Handle all requests on w until it's closed.
 // Creating a custom BucketRunner allows managing extra resources per bucket.
-type BucketRunner func (id BucketId, w chan WorkFunc)
-type BucketRunnerWithState[T any] func (id BucketId, w chan WorkFuncWithState[T])
+type BucketRunner[Id comparable, State any] func (id Id, w chan WorkFunc[Id, State])
 
-func defaultBucketRunner(id BucketId, w chan WorkFunc) {
-	for f := range w {
-		f()
-	}
-}
-
-func defaultBucketRunnerWithState[T any](id BucketId, w chan WorkFuncWithState[T]) {
-	var state T
+func defaultBucketRunner[Id comparable, State any](id Id, w chan WorkFunc[Id, State]) {
+	var state State
 
 	for f := range w {
 		f(&state)
 	}
 }
 
-func handleBucketWorkReq(done chan BucketId, buckets map[BucketId]BucketData, runner BucketRunner, req BucketWorkReq) {
+func handleBucketWorkReq[Id comparable, State any](done chan Id, buckets map[Id]BucketData[Id, State], runner BucketRunner[Id, State], req BucketWorkReq[Id, State]) {
 	n, ok := buckets[req.Bucket]
 	if ! ok {
-		n.work = make(chan WorkFunc, 4)
+		n.work = make(chan WorkFunc[Id, State], 4)
 		jobs := uint(0)
 		n.jobs = &jobs
 
@@ -59,28 +38,7 @@ func handleBucketWorkReq(done chan BucketId, buckets map[BucketId]BucketData, ru
 	}
 
 	*n.jobs = *n.jobs + 1
-	n.work <- func () {
-		defer func() {
-			done <- req.Bucket
-		}()
-		req.F()
-	}
-}
-
-func handleBucketWorkReqWithState[T any](done chan BucketId, buckets map[BucketId]BucketDataWithState[T], runner BucketRunnerWithState[T], req BucketWorkReqWithState[T]) {
-	n, ok := buckets[req.Bucket]
-	if ! ok {
-		n.work = make(chan WorkFuncWithState[T], 4)
-		jobs := uint(0)
-		n.jobs = &jobs
-
-		buckets[req.Bucket] = n
-
-		go runner(req.Bucket, n.work)
-	}
-
-	*n.jobs = *n.jobs + 1
-	n.work <- func (state *T) {
+	n.work <- func (state *State) {
 		defer func() {
 			done <- req.Bucket
 		}()
@@ -88,23 +46,7 @@ func handleBucketWorkReqWithState[T any](done chan BucketId, buckets map[BucketI
 	}
 }
 
-func handleDone(buckets map[BucketId]BucketData, bId BucketId) {
-	n, ok := buckets[bId]
-	if ! ok {
-		return
-	}
-
-	*n.jobs = *n.jobs - 1
-
-	if *n.jobs > 0 {
-		return
-	}
-
-	close(n.work)
-	delete(buckets, bId)
-}
-
-func handleDoneWithState[T any](buckets map[BucketId]BucketDataWithState[T], bId BucketId) {
+func handleDone[Id comparable, State any](buckets map[Id]BucketData[Id, State], bId Id) {
 	n, ok := buckets[bId]
 	if ! ok {
 		return
@@ -122,9 +64,9 @@ func handleDoneWithState[T any](buckets map[BucketId]BucketDataWithState[T], bId
 
 // Handles requests from c until closed.
 // Waits for all workers to finish before returning.
-func Bucket(c chan BucketWorkReq, runner BucketRunner) {
-	buckets := make(map[BucketId]BucketData)
-	done := make(chan BucketId)
+func Bucket[Id comparable, State any](c <-chan BucketWorkReq[Id, State], runner BucketRunner[Id, State]) {
+	buckets := make(map[Id]BucketData[Id, State])
+	done := make(chan Id)
 	defer close(done)
 
 	for {
@@ -154,42 +96,12 @@ finish:
 	}
 }
 
-func BucketWithState[T any](c chan BucketWorkReqWithState[T], runner BucketRunnerWithState[T]) {
-	buckets := make(map[BucketId]BucketDataWithState[T])
-	done := make(chan BucketId)
-	defer close(done)
-
-	for {
-		select {
-		case req, ok := <-c:
-			if ! ok {
-				goto finish
-			}
-
-			handleBucketWorkReqWithState(done, buckets, runner, req)
-		case bId := <-done:
-			handleDoneWithState(buckets, bId)
-		}
-	}
-
-finish:
-	if len(buckets) == 0 {
-		return
-	}
-
-	for bId := range done {
-		handleDoneWithState(buckets, bId)
-
-		if len(buckets) == 0 {
-			return
-		}
-	}
+func DefaultBucket[Id comparable, State any](c <-chan BucketWorkReq[Id, State]) {
+	Bucket(c, defaultBucketRunner[Id, State])
 }
 
-func DefaultBucket(c chan BucketWorkReq) {
-	Bucket(c, defaultBucketRunner)
-}
-
-func DefaultBucketWithState[s T](c chan BucketWorkReqWithState[T]) {
-	Bucket(c, defaultBucketRunnerWithState[T])
+func SpawnDefaultBucket[Id comparable, State any]() (chan<- BucketWorkReq[Id, State]) {
+	c := make(chan BucketWorkReq[Id, State])
+	go DefaultBucket(c)
+	return c
 }
